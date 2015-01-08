4d68768fc4aec71d8eedeecece78c64ff65cc479
Justin Herrick <justin@justinherrick.com>
2015-01-08 14:25:02 -0600

    Update blackjack.rb
    Good job leveraging classes for this assignment. Be careful with your capitalization and watch to make sure method and class is "closed" (with and end) at the spot you want it to. I made a few comments of areas that could be improved and to point out errors you may be having. I hope this helps. I look forward to trying out your game once it is up and running. 
    
    (Don't worry about making these changes this weekend. We will talk more about this on Monday)

--- blackjack.rb
+++ blackjack.rb
@@ -5,9 +5,10 @@ puts "-" * 20
 
 class Blackjack
   def initialize
-    @player = player.new
-    @dealer = dealer.new
+    @player = player.new # You are calling .new on a variable (which has not been set, so it is nil) and not on the Class - JH
+    @dealer = dealer.new # You are calling .new on a variable (which has not been set, so it is nil) and not on the Class - JH
   end
+  #<< You're not ending your Blackjack class, which means the rest of the file is *within* that class.
 
 class Gambler
   attr_accessor :cards, :bank
@@ -21,7 +22,7 @@ class Deck
 
   attr_accessor :cards
   def initialize
-    @cards = ((1..11).to_a * 4).shuffle
+    @cards = ((1..11).to_a * 4).shuffle #Missing a few cards in your deck - JH
   end
 
   def count
@@ -36,13 +37,13 @@ class Deck
     @cards.shift
   end
 end
-end
+end # << This is the end of your Blackjack class - JH
 
 
 # Player draws 2 cards and dealer draws 2 cards
 # Each round will cost player $100
-  def play
-    while @player.cards.count > 0 do
+  def play # << This method doesnt exist in any class - JH
+    while @player.cards.count > 0 do # @player is going to be not defined out here - JH
       first_card = @player.cards.draw
       second_card = @player.cards.draw
       round = @bank - 10
@@ -59,7 +60,7 @@ end
         puts "You lost kid."
       elsif third_card + fourth_card > 21
         puts "House loses."
-
+     #You don't need all these extra lines down here before your end -JH
 
     end
   end
